e89319cb42800d522fbb6eb332706c739439d834
Matthew Giorno <32398671+Negative-Feedback@users.noreply.github.com>
2020-12-06 17:59:15 -0500

    Add files via upload
    Updated the schema to include password hashes and email addresses

--- masterscript.sql
+++ masterscript.sql
@@ -3,14 +3,9 @@ CREATE DATABASE FakeUni;
 USE FakeUni;
 
 create table login(
-loginid varchar(50),
-pass varchar(50),
+loginId varchar(50),
+pass varbinary(64),
 username varchar(64),
-title varchar(32),
+email varchar(64),
 PRIMARY KEY (loginid)
 );
-
-insert into login
-VALUES ('mGiorno', 'password', 'Matthew Giorno', 'DBA');
-insert into login
-VALUES ('kGenereux', 'password', 'Kevin Genereux', 'DBA');
